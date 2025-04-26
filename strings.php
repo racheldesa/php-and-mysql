61c199bfb5c3aed0d12ddae1369d6ce6d3282a6d
racheldesa <racheldesa@outlook.com>
2025-04-26 09:30:46 -0500

    change file ext strings.html to strings.php

---
+++ strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <title>Strings</title>
+</head>
+<body>
+    <?php # Script 1.7 - strings.php
+
+        // Create the variables.
+        $first_name = 'Nick';
+        $last_name = 'Hornby';
+        $book = 'High Fidelity';
+
+        // Print the values
+        echo "The book <i>$book</i> was written by $first_name $last_name";
+
+    ?>
+</body>
+</html>
